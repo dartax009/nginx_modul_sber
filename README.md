32356630f849a3e518fb25b4ec5de6caf414d0f6
dartax009 <dartax009@gmail.com>
2024-12-07 19:45:58 +0300

    Изменил контекст исполнения директивы. Описал README

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# nginx_modul_sber
+# Сборка модуля с ядром nginx
+
+Для сборки модуля с ядром nginx необходимо выполнить команду
+
+```
+./configure --add-module=путь/до/модуля/nginx_modul_sber
+```
+# Описание директивы
+
+Для работы верификации json при регистрации персонажа, необходимо включить следующую директиву:
+
+```
+location /check_json {
+    character_shem_check on;
+}
+```
+
+Регулировка работы модуля осуществляется написанием "on", "off" у директивы character_shem_check.
+
+# Валидируемые схемы json
+
+В рамках данного пет-проекта, модуль валидирует следующую схему json:
+```
+{
+    "name": "Aragorn",
+    "race": "Human",
+    "ch_class": "Ranger",
+    "level": 5,
+    "hit_points":
+    {
+        "max": 40,
+        "current": 35
+    },
+    "abilities":
+    {
+        "strength": 18,
+        "dexterity": 13,
+        "constitution": 16,
+        "intelligence": 12,
+        "wisdom": 14,
+        "charisma": 11
+    },
+    "skills": ["Survival", "Analysis", "Stealth"]
+}
+```
+# Пример запроса
+
+Далее будут приведен пример http запросы для оценки работоспособности модуля:
+
+```
+curl -X POST http://localhost/check_json \
+     -H "Content-Type: application/json" \
+     -d '{
+          "name": "Aragorn",
+          "race": "Human",
+          "ch_class": "Ranger",
+          "level": 5,
+          "hit_points": {
+              "max": 40,
+              "current": 35
+          },
+          "abilities": {
+              "strength": 18,
+              "dexterity": 13,
+              "constitution": 16,
+              "intelligence": 12,
+              "wisdom": 14,
+              "charisma": 11
+          },
+          "skills": ["Survival", "Analysis", "Stealth"]
+      }'
+```
+Ожидаемый ответ:
+
+HTTP/1.1 200 OK
+
+Content-Type: text/plain
+
+Тело: The character is saved
--- sber_modul.c
+++ sber_modul.c
@@ -5,10 +5,8 @@
 
 #include "ver_json/check_shem_json.h"
 
-
 static ngx_int_t ngx_http_sber_modul_handler(ngx_http_request_t *r);
-static ngx_int_t ngx_http_content_sber_modul_init (ngx_conf_t *cf);
-static char *ngx_http_conf_sber_modul_init (ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static char *ngx_http_character_shem_check (ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static uint8_t check_on = 0;    ///< 0 - выключена обработка, 1 - включена обработка
 
@@ -19,8 +17,8 @@ typedef struct
 
 static ngx_command_t ngx_http_sber_modul_commands[] = {
     { ngx_string("character_shem_check"),
-      NGX_CONF_TAKE1|NGX_DIRECT_CONF|NGX_ANY_CONF,
-      ngx_http_conf_sber_modul_init,
+      NGX_HTTP_LOC_CONF| NGX_CONF_FLAG,
+      ngx_http_character_shem_check,
       0,
       0,
       NULL },
@@ -29,7 +27,7 @@ static ngx_command_t ngx_http_sber_modul_commands[] = {
 
 static ngx_http_module_t ngx_http_sber_modul_module_ctx = {
     NULL,                          /* preconfiguration */
-    ngx_http_content_sber_modul_init,                          /* postconfiguration */
+    NULL,                          /* postconfiguration */
 
     NULL,                          /* create main configuration */
     NULL,                          /* init main configuration */
@@ -41,7 +39,7 @@ static ngx_http_module_t ngx_http_sber_modul_module_ctx = {
     NULL                           /* merge location configuration */
 };
 
-ngx_module_t ngx_http_sber_modul_module = {
+ngx_module_t ngx_http_sber_modul_module = {     //TODO 
     NGX_MODULE_V1,
     &ngx_http_sber_modul_module_ctx,  /* module context */
     ngx_http_sber_modul_commands,     /* module directives */
@@ -56,110 +54,19 @@ ngx_module_t ngx_http_sber_modul_module = {
     NGX_MODULE_V1_PADDING
 };
 
-static void ngx_http_sber_body_hendler(ngx_http_request_t *r)
+
+static char* ngx_http_character_shem_check(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) 
 {
-    ngx_buf_t *buf;
-    ngx_chain_t out;
-    ngx_int_t rc;
-    ngx_str_t response;
-    ngx_int_t status = NGX_HTTP_OK;
+    ngx_http_core_loc_conf_t *clcf;
 
-    if ( check_shem((char *)r->request_body->bufs->buf->pos, r->request_body->bufs->buf->last - r->request_body->bufs->buf->pos) == 1 )
-    {
-        ngx_http_finalize_request(r, NGX_HTTP_UNSUPPORTED_MEDIA_TYPE);
-        return;
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+
+    if (clcf == NULL) {
+        return NGX_CONF_ERROR;
     }
-    else
-    {
-        response.data = (u_char *)"The character is saved";
-        response.len = sizeof("The character is saved")-1;
-    }
+    clcf->handler = ngx_http_sber_modul_handler;
+    
 
-    r->headers_out.status = status;
-    r->headers_out.content_length_n = response.len;
-
-    rc = ngx_http_send_header(r);
-    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
-    {
-        ngx_http_finalize_request(r, rc);
-        return;
-    }
-
-    buf = ngx_create_temp_buf(r->pool, response.len);
-    if (buf == NULL)
-    {
-        ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
-        return;
-    }
-
-    ngx_memcpy(buf->pos, response.data, response.len);
-    buf->last = buf->pos + response.len;
-    buf->last_buf = 1;
-
-    out.buf = buf;
-    out.next = NULL;
-
-    ngx_http_output_filter(r, &out);
-}
-
-static ngx_int_t ngx_http_sber_modul_handler(ngx_http_request_t *r)
-{
-    ngx_int_t rc;
-    ngx_str_t json_content_type = ngx_string("application/json");
-    u_char *content_type_start;
-
-    if (check_on == 0)
-    {
-        return NGX_DECLINED; // Чтобы продолжить дальнейший конвеер обработки возвращаем DECLINED
-    }
-
-    if (r->method != NGX_HTTP_POST) {
-        return NGX_DECLINED;
-    }
-
-    if (r->headers_in.content_type == NULL)
-    {
-        return NGX_DECLINED;
-    }
-    else
-    {
-        content_type_start = r->headers_in.content_type->value.data;
-
-        if (ngx_strncmp(content_type_start, (char *)json_content_type.data, json_content_type.len - 1) != 0)
-        {
-            return NGX_DECLINED;
-        }
-    }
-
-    rc = ngx_http_read_client_request_body(r, ngx_http_sber_body_hendler);
-    if (rc >= NGX_HTTP_SPECIAL_RESPONSE) {
-        return rc;
-    }
-
-    return NGX_DONE;
-}
-
-static ngx_int_t ngx_http_content_sber_modul_init (ngx_conf_t *cf)
-{
-    ngx_http_handler_pt *h = NULL;
-    ngx_http_core_main_conf_t *cmcf = NULL;
-
-
-    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
-
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
-    if (h == NULL)
-    {
-        return NGX_ERROR;
-    }
-
-    (*h) = ngx_http_sber_modul_handler;
-
-    return NGX_OK;
-}
-
-static char *ngx_http_conf_sber_modul_init (ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
-{
     ngx_str_t *value = cf->args->elts;
 
     if (ngx_strncmp((char *)value[1].data, "on", sizeof("on")-1) == 0)
@@ -170,8 +77,99 @@ static char *ngx_http_conf_sber_modul_init (ngx_conf_t *cf, ngx_command_t *cmd, 
 
     if (ngx_strncmp((char *)value[1].data, "off", sizeof("off")-1) == 0)
     {
+        check_on = 0;
         return NGX_CONF_OK;
     }
 
     return NGX_CONF_ERROR;
+}
+
+static void ngx_http_sber_finalize_request(ngx_http_request_t *r, ngx_str_t* response) 
+{
+    ngx_buf_t *buf;
+    ngx_chain_t out;
+    ngx_int_t rc;
+
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = response->len;
+    r->headers_out.content_type_len = sizeof("text/plain") - 1;
+    ngx_str_set(&r->headers_out.content_type, "text/plain");
+    r->headers_out.content_type_lowcase = NULL;
+
+    rc = ngx_http_send_header(r);
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
+    {
+        ngx_http_finalize_request(r, rc);
+        return;
+    }
+
+    buf = ngx_create_temp_buf(r->pool, response->len);
+    if (buf == NULL)
+    {
+        ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
+        return;
+    }
+
+    ngx_memcpy(buf->pos, response->data, response->len);
+    buf->last = buf->pos + response->len;
+    buf->last_buf = 1;
+
+    out.buf = buf;
+    out.next = NULL;
+
+    ngx_http_output_filter(r, &out);
+}
+
+static void ngx_http_sber_body_hendler(ngx_http_request_t *r)
+{
+    if ( check_shem((char *)r->request_body->bufs->buf->pos, r->request_body->bufs->buf->last - r->request_body->bufs->buf->pos) == 1 )
+    {
+        ngx_http_finalize_request(r, NGX_HTTP_UNSUPPORTED_MEDIA_TYPE);
+        return;
+    }
+    
+    ngx_http_sber_finalize_request(r, &(ngx_str_t)ngx_string("The character is saved\n"));
+}
+
+static ngx_int_t ngx_http_sber_modul_handler(ngx_http_request_t *r)
+{
+    ngx_int_t rc;
+    ngx_str_t json_content_type = ngx_string("application/json");
+    u_char *content_type_start;
+
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,"check - %d", check_on);
+    if (check_on == 0)
+    {
+        ngx_http_sber_finalize_request(r, &(ngx_str_t)ngx_string("The character is saved\n"));
+        return NGX_DONE;
+    }
+
+    if (r->method != NGX_HTTP_POST) {
+        ngx_http_finalize_request(r, NGX_HTTP_BAD_REQUEST);
+        return NGX_ERROR;
+    }
+
+    if (r->headers_in.content_type == NULL)
+    {
+        ngx_http_finalize_request(r, NGX_HTTP_BAD_REQUEST);
+        return NGX_ERROR;
+    }
+    else
+    {
+        content_type_start = r->headers_in.content_type->value.data;
+
+        if (ngx_strncmp(content_type_start, (char *)json_content_type.data, json_content_type.len - 1) != 0)
+        {
+            ngx_http_finalize_request(r, NGX_HTTP_BAD_REQUEST);
+            return NGX_ERROR;
+        }
+    }
+
+    rc = ngx_http_read_client_request_body(r, ngx_http_sber_body_hendler);
+    if (rc >= NGX_HTTP_SPECIAL_RESPONSE) 
+    {
+        return rc;
+    }
+
+    return NGX_DONE;
 }
